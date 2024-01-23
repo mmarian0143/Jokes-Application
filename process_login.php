fc459003f6a121783a10a6292e75ad791a99eb65
Madalina Marian <82545182+mmarian0143@users.noreply.github.com>
2024-01-22 21:57:58 -0700

    Update process_login.php
    updating process_login.php code to allow login correctly

--- process_login.php
+++ process_login.php
@@ -15,52 +15,52 @@ $password = $_POST['password'];
 
 echo "<p>You attempted to login with " . $username . " and " . $password . "</p>";
 
+$sql = "SELECT user_id, user_name, password FROM users WHERE user_name =  '$username' AND password = '$password'";
+
+echo "SQL = " . $sql . "<br>";
+
+$result = $mysqli->query($sql);
+
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+
 $stmt = $mysqli->prepare ("SELECT user_id, user_name, password FROM users WHERE user_name = ?");
 $stmt->bind_param("s", $username);
 
 $stmt->execute();
 $stmt->store_result();
 
-$stmt->bind_result($userid, $username, $pw);
-
-//$sql = "SELECT user_id, user_name, password FROM users WHERE user_name =  '$username' AND password = '$password'";
-
-//echo "SQL = " . $sql . "<br>";
+$stmt->bind_result($userid, $fetched_name, $fetched_pass);
 
 
-if ($stmt->num_rows == 1 ) {     
-    echo "I found one person with that username<br>";
+ if ($stmt->num_rows <= 1 ) {
+    echo "Found 1 person with that username<br>";
     $stmt->fetch();
-    if (password_verify($password, $pw)) {
-        echo "The password matches<br>";
-        echo "Login successul<br>";
+
+    if (($password == $fetched_pass)) {
+        echo " pw matches<br>";
+        echo "<p>Login success</p>"; 
         $_SESSION['username'] = $username;        
         $_SESSION['userid'] = $userid;  
         exit;
     }
     else {
-    $_SESSION = [];    
-        session_destroy();
+    echo "Password does not match<br>";
     }
 }
     else {    
 
+        echo "0 results. Not logged in<br>";
         $_SESSION = [];    
         session_destroy();
     }
-    echo "Login failed<br>";
+   echo "Session variable = ";
+print_r($_SESSION);
 
-    echo "Session variable = <br>";
-    echo "</pre>";
-    print_r($_SESSION);
-    echo "</pre>";
+echo "<br>";
 
-    echo "<br><a href='index.php'>Return to main page</a>";
-
-    
+echo "<a href='index.php'>Return to main page</a>";
 ?>
 
 </html>
-
-
-
