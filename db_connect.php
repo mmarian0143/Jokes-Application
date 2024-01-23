936eda7d9848d54e05e4d15889653c8c28c4d34e
Madalina Marian <82545182+mmarian0143@users.noreply.github.com>
2024-01-22 21:17:58 -0700

    Update db_connect.php
    Updated DB credentials to use Heroku instead of localhost

--- db_connect.php
+++ db_connect.php
@@ -4,11 +4,11 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "127.0.0.1";
+$host = "xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $port = "3306";
-$username = "db_admin";
-$user_pass = "password";
-$database_in_use = "JokesPart1";
+$username = "lshlp239yp7dm32v";
+$user_pass = "jccgihxf1rpnlwpq";
+$database_in_use = "dsvmuw2eonbjy51h";
 
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
